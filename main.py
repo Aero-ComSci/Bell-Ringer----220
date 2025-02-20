cb5a8684d2934b848d052f2468498ed8b1c5237b
YukthikaP <148280173+YukthikaP@users.noreply.github.com>
2025-02-20 13:33:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ Fix the lyrics First to get their pulled request wins!
 
 [Chorus] O and l, coding like a pro, Turning base 10, break it down slow. Binary flow, digital on deck, Watch me flex, conversion so tech.
 
-[Verse 2] ll, that's a three in disguise, Four's just two-oh, woke up to fries. Five got a ring, one-o-one bling, Encode the numbers, hear the bytes sing.
+[Verse 2] ll, that's a three in disguise, Four's just l-o-o, woke up to fries. Five got a ring, one-o-one bling, Encode the numbers, hear the bytes sing.
 
 [Bridge] Count 'em, calculate, algorithm slick, Tech cypher, be sharp and quick. Switch 'em, flip 'em, binary waves, Digital hustle, modern day slaves.
 
-[Verse 3] Six is triple down, double-oh-l, Seven stacks up, o-o-l, swell. Eight a fresh start, one more bit, Nine o-o-l-o, making it lit.
+[Verse 3] Six is triple down, double-l-o, Seven stacks up, l-l-l, swell. Eight a fresh start, one more bit, Nine l-o-o-l, making it lit.
 
 [Chorus] O and l, coding like a pro, Turning base 10, break it down slow. Binary flow, digital on deck, Watch me flex, conversion so tech.
